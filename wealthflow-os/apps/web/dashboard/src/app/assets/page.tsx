0f590a9cccd5099caa8c49b6e68fea2d590493b4
hazemhishamelshorbagy <hazem.frontend@gmail.com>
2026-04-09 13:08:54 +0200

    wealthflow-modulefederation

---
+++ wealthflow-os/apps/web/dashboard/src/app/assets/page.tsx
@@ -0,0 +1,5 @@
+const AssetsPage = () => {
+  return <div>Welcome to Assets Management Page</div>;
+};
+
+export default AssetsPage; // ضروري جداً ليعمل الـ Lazy Loading
---
+++ wealthflow-os/libs/shared/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,9 @@
+export function WealthflowOsUiComponents() {
+  return (
+    <div>
+      <h1>Welcome to WealthflowOsUiComponents!</h1>
+    </div>
+  );
+}
+
+export default WealthflowOsUiComponents;
