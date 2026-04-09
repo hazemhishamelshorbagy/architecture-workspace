1f1c7a82eb556785cd8df5d68f612f1b116e1478
hazemhishamelshorbagy <hazem.frontend@gmail.com>
2026-04-09 15:32:07 +0200

    implement design system and husky and lintedstage

--- wealthflow-os/apps/web/tailwind.config.js
+++ wealthflow-os/apps/web/tailwind.config.js
@@ -8,7 +8,7 @@
 
 // If you are **not** using `--turbo` you can uncomment both lines 1 & 19.
 // A discussion of the issue can be found: https://github.com/nrwl/nx/issues/26510
-
+const { themeTokens } = require('../../shared/design-system/src/lib/design-system');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -17,7 +17,47 @@ module.exports = {
     //     ...createGlobPatternsForDependencies(__dirname)
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // سيقوم Tailwind بتوليد كلاسات مثل bg-brand-primarydark أو text-brand-accentgreen
+        brand: themeTokens.colors.brand,
+        text: themeTokens.colors.text,
+        border: themeTokens.colors.border,
+      },
+      fontSize: {
+        'display': [themeTokens.typography.display.fontSize, {
+          lineHeight: themeTokens.typography.display.lineHeight,
+          fontWeight: themeTokens.typography.display.fontWeight,
+        }],
+        'h1': [themeTokens.typography.h1.fontSize, {
+          lineHeight: themeTokens.typography.h1.lineHeight,
+          fontWeight: themeTokens.typography.h1.fontWeight,
+        }],
+        'h2': [themeTokens.typography.h2.fontSize, {
+          lineHeight: themeTokens.typography.h2.lineHeight,
+          fontWeight: themeTokens.typography.h2.fontWeight,
+        }],
+        'body-lg': [themeTokens.typography.bodyLg.fontSize, {
+          lineHeight: themeTokens.typography.bodyLg.lineHeight,
+        }],
+        'body': [themeTokens.typography.body.fontSize, {
+          lineHeight: themeTokens.typography.body.lineHeight,
+        }],
+        'body-sm': [themeTokens.typography.bodySm.fontSize, {
+          lineHeight: themeTokens.typography.bodySm.lineHeight,
+        }],
+        'label': [themeTokens.typography.label.fontSize, {
+          lineHeight: themeTokens.typography.label.lineHeight,
+          fontWeight: themeTokens.typography.label.fontWeight,
+        }],
+      },
+      borderRadius: themeTokens.borderRadius,
+      boxShadow: themeTokens.shadows,
+      fontFamily: {
+        'mono': [themeTokens.typography.mono.fontFamily],
+
+      }
+    }
   },
   plugins: [],
 };
